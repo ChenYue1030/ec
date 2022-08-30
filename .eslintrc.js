166b4b56817c1c19fa3e00ee6d7920a74fb6c235
yue.chen <1147542926@qq.com>
2022-08-30 09:57:55 +0800

    test: add unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    project: "./tsconfig.json",
+    // project: "./tsconfig.json",
+    project: './tsconfig.eslint.json'
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
