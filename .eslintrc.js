f8b9dc4c04e16ac5d7d037f2a3322e997a579f3f
yue.chen <1147542926@qq.com>
2022-08-30 09:47:15 +0800

    ci: add eslint and commitlint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'eslint-config-standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  },
+};
